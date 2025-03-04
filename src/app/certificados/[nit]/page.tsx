bce006ac5627395e35c7053a3785f494bfa32e57
jldev1227 <1227jldev@gmail.com>
2025-03-04 15:53:12 -0500

    first commit

---
+++ src/app/certificados/[nit]/page.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import axios from "axios";
+
+interface Params {
+  params: { nit: string };
+}
+
+async function fetchDocuments(nit: string) {
+  try {
+    const response = await axios.get(`${process.env.API_BASE_URL}/empresas/${nit}`);
+    return response.data.documentos;
+  } catch (error) {
+    console.error("Error al obtener documentos:", error);
+    return [];
+  }
+}
+
+export default async function ResponsiveImage({ params }: Params) {
+  const nit = await params.nit; // Desestructuración asíncrona para evitar el acceso directo
+  const documents = await fetchDocuments(nit);
+
+  return (
+    <div className="flex flex-col sm:bg-[url('/background2.svg')] bg-cover bg-center sm:p-10 gap-16">
+      <div className="flex justify-center items-center">
+        <Image
+          src="/CodyBannerIzq.png"
+          width={200}
+          height={200}
+          alt="Cody - Mascota de Transmeralda"
+        />
+      </div>
+      <div className="space-y-10 flex-col items-center bg-white rounded-xl shadow-xl p-4 md:p-10">
+        <h2 className="text-lg font-semibold text-center">Documentos de la empresa con NIT: {nit}</h2>
+        <ul className="space-y-5">
+          {documents.length > 0 ? (
+            documents.map((doc: { nombre: string; url: string }, index: number) => (
+              <li key={index} className="text-blue-500 underline shadow-sm border border-gray-200 p-4 rounded-md">
+                <a className="flex items-center gap-5 max-sm:text-xs" href={doc.url} target="_blank" rel="noopener noreferrer">
+                  <img
+                    src="/pdf_icon.png"
+                    width={48}
+                    height={48}
+                    className="w-[32] h-[32] md:w-[48px] md:h-[48px]"
+                    alt="Icono de archivo"
+                  />
+                  {doc.nombre}
+                </a>
+              </li>
+            ))
+          ) : (
+            <p>No hay documentos disponibles.</p>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,65 @@
+"use client";
+
 import Image from "next/image";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [nit, setNit] = useState('')
+  const router = useRouter()
+
+  const handleNitChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNit(e.target.value)
+  }
+  
+  const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if(nit === '') {
+      alert('El campo NIT no puede estar vacio')
+      return
+    }
+
+    router.push(`/certificados/${nit}`)
+
+  }
+
+  return (
+    <div className="h-screen flex flex-col lg:flex-row items-center justify-center sm:bg-[url('/background2.svg')] bg-cover bg-center sm:p-20">
+      <div className="w-full space-y-5 flex flex-col bg-white p-10 rounded-md sm:shadow-md">
+        <div className="space-y-2">
+          <h1 className="text-4xl text-left text-green-700">¡Bienvenido!</h1>
+          <p className="text-md md:text-4xl text-left">Consulta todos tus certificados aqui</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div>
+          <form onSubmit={handleSubmit} className="space-y-10">
+            <div className="flex flex-col space-y-2">
+              <label className="text-md text-left">NIT de tu empresa</label>
+              <input
+                type="number"
+                value={nit}
+                onChange={handleNitChange}
+                min={0}
+                step={1}
+                className="p-2 border border-gray-300 rounded-md focus:border-green-600 outline-none"
+              />
+
+            </div>
+            <button type="submit" className="bg-green-700 w-full p-3 text-white rounded-md">Ver certificados</button>
+          </form>
+        </div>
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <Image
+          src="/CodyBannerIzq.png"
+          width={400}
+          height={400}
+          alt="Cody - Mascota de Transmeralda"
+          className="w-[250px] h-[300px] md:w-[350] md:h-[400]"
+        />
+      </div>
     </div>
   );
 }
